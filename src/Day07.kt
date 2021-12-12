eff0f3aa3ed82bdae06af8d2206180c07e577002
IMAI Kazuki <imaikazuki@gmx.com>
2021-12-12 19:14:10 +0900

    Complete Day 7.

---
+++ src/Day07.kt
@@ -0,0 +1,30 @@
+import kotlin.math.abs
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        val positions = input.first().split(",").map { it.toInt() }
+        val min = positions.minOf { it }
+        val max = positions.maxOf { it }
+        return (min..max)
+            .map { goal -> positions.map { abs(goal - it) }.sumOf { it } }
+            .minOf { it }
+    }
+
+    fun part2(input: List<String>): Int {
+        val positions = input.first().split(",").map { it.toInt() }
+        val min = positions.minOf { it }
+        val max = positions.maxOf { it }
+        return (min..max)
+            .map { goal -> positions.map { (0..abs(goal - it)).sumOf { n -> n } }.sumOf { it } }
+            .minOf { it }
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day07_test")
+    check(part1(testInput) == 37)
+    check(part2(testInput) == 168)
+
+    val input = readInput("Day07")
+    println(part1(input))
+    println(part2(input))
+}
